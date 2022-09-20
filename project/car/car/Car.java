05a6cdab874649fb085444618f461a60818f78b2
PrometheusUA <prometheusua@i.ua>
2022-09-20 20:50:01 +0300

    Changed file structure, Runner and task a implemented

--- project/car/car/Car.java
+++ project/car/car/Car.java
@@ -6,7 +6,7 @@ public class Car{
   private int cost;
   private String registrationNumber;
 
-  public Car(int id, String brand, int yearOfIsseue, String color, int cost, String registrationNumber){
+  public Car(int id, String brand, int yearOfIssue, String color, int cost, String registrationNumber){
     this.id = id;
     this.brand = brand;
     this.yearOfIssue = yearOfIssue;
@@ -45,5 +45,10 @@ public class Car{
         "Car [\nid: %d. \nbrand: %s \nyear of issue: %d \ncolor: %s \ncost: %d \nregistration number: %s \n]",
         this.id, this.brand, this.yearOfIssue, this.color, this.cost, this.registrationNumber
       ));
+  }
+
+  public static void main(String[] args){
+    Car car = new Car(1, "Toyota", 2019, "black", 150000, "AA 0001 AA");
+    car.print();
   }
 }
---
+++ project/car/runner/Runner.java
@@ -0,0 +1,9 @@
+import Car;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args){
+        Car car = new Car(1, "Toyota", 2019, "black", 150000, "AA 0001 AA");
+        car.print();
+    }
+}
