a2901bfe635af1db5b1e3163280b4b04a097a837
Liudomyrskyi Yurii <90156877+linkdew@users.noreply.github.com>
2022-09-21 17:35:01 +0300

    Delete project/car directory

--- project/car/runner/Runner.java
+++
@@ -1,9 +0,0 @@
-import Car;
-import java.util.List;
-
-public class Runner {
-    public static void main(String[] args){
-        Car car = new Car(1, "Toyota", 2019, "black", 150000, "AA 0001 AA");
-        car.print();
-    }
-}
