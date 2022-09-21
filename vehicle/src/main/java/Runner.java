353b3e3c64598854adf320c7644b23c3268bc5ec
linkdew <you@example.com>
2022-09-21 23:40:51 +0300

    done

--- vehicle/src/main/java/Runner.java
+++ vehicle/src/main/java/Runner.java
@@ -5,14 +5,12 @@ import main.java.entity.Car;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.time.Year;
+import java.util.*;
 
 public class Runner {
     private static void taskA(BufferedReader reader, List<Car> cars){
-        System.out.print("Input brand you want to get cars from: ");
+        System.out.print("Enter brand you want to get cars from: ");
         try {
             String brand = reader.readLine();
             List<Car> brandedCars = new ArrayList<>();
@@ -28,7 +26,7 @@ public class Runner {
             else{
                 System.out.println("Cars from this brand:");
                 for (Car tempCar: brandedCars){
-                    tempCar.print();
+                    System.out.println(tempCar.toString());
                 }
             }
         }
@@ -38,17 +36,70 @@ public class Runner {
         }
     }
 
-    public static void main(String[] args) {
-        Car car = new Car(1, "Toyota", 2019, "black", 150000, "AA 0001 AA");
-        Car car2 = new Car(2, "Toyota", 2015, "blue", 50000, "AM 8288 KI");
-        Car car3 = new Car(3, "Bentley", 2022, "white", 350000, "AE 9889 EA");
-        Car car4 = new Car(4, "Toyota", 2018, "black", 120000, "AK 1209 II");
-        Car car5 = new Car(5, "Mercedes", 2015, "blue", 50000, "AM 8288 KI");
-        Car car6 = new Car(6, "Mercedes", 2015, "blue", 50000, "AA 8989 KI");
-        Car car7 = new Car(7, "Ford", 2010, "black", 42000, "AE 9527 EE");
-        Car car8 = new Car(8, "Ford", 2012, "white", 110000, "AU 7712 LH");
-        Car car9 = new Car(9, "Ford", 2016, "blue", 98000, "AA 1231 AU");
-        Car car10 = new Car(10, "Toyota", 2007, "red", 45000, "AM 1212 AM");
+    public static void taskB(BufferedReader reader, List<Car> cars) throws IOException {
+        int currentYear = Year.now().getValue();
+
+        System.out.print("Enter model of the car: ");
+        String model = reader.readLine();
+
+        System.out.print("Specify amount of years: ");
+        int yearsN = Integer.parseInt(reader.readLine());
+
+        List<Car> selectedCars = new ArrayList<>();
+
+        for (Car tempCar: cars){
+            if(Objects.equals(tempCar.getModel(), model) && (currentYear - tempCar.getYearOfIssue()> yearsN)) {
+                selectedCars.add(tempCar);
+            }
+        }
+
+        if(selectedCars.size() == 0)
+            System.out.println("no car fulfils a condition");
+        else {
+            System.out.println("selected cars:");
+            for (Car tempCar: selectedCars){
+                System.out.println(tempCar.toString());
+            }
+        }
+    }
+
+
+    public static void taskC(BufferedReader reader, List<Car> cars) throws IOException {
+        System.out.print("Enter year of issue of the car: ");
+        int yearOfIssue = Integer.parseInt(reader.readLine());
+
+        System.out.print("Specify price: ");
+        int cost = Integer.parseInt(reader.readLine());
+
+        List<Car> selectedCars = new ArrayList<>();
+
+        for (Car tempCar: cars){
+            if(Objects.equals(tempCar.getYearOfIssue(), yearOfIssue) && (tempCar.getCost() > cost)) {
+                selectedCars.add(tempCar);
+            }
+        }
+
+        if(selectedCars.size() == 0)
+            System.out.println("no car fulfils a condition");
+        else {
+            System.out.println("selected cars:");
+            for (Car tempCar: selectedCars){
+                System.out.println(tempCar.toString());
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Car car = new Car(1, "Toyota", "A1", 2019, "black", 150000, "AA 0001 AA");
+        Car car2 = new Car(2, "Toyota", "A2", 2015, "blue", 50000, "AM 8288 KI");
+        Car car3 = new Car(3, "Bentley", "B1",2022, "white", 350000, "AE 9889 EA");
+        Car car4 = new Car(4, "Toyota", "A1", 2018, "black", 120000, "AK 1209 II");
+        Car car5 = new Car(5, "Mercedes", "Flex", 2015, "blue", 50000, "AM 8288 KI");
+        Car car6 = new Car(6, "Mercedes", "Help", 2015, "blue", 50000, "AA 8989 KI");
+        Car car7 = new Car(7, "Ford", "Flex", 2010, "black", 42000, "AE 9527 EE");
+        Car car8 = new Car(8, "Ford", "Mustang", 2012, "white", 110000, "AU 7712 LH");
+        Car car9 = new Car(9, "Ford", "Mustang", 2016, "blue", 98000, "AA 1231 AU");
+        Car car10 = new Car(10, "Toyota", "B1", 2007, "red", 45000, "AM 1212 AM");
 
         List<Car> cars = Arrays.asList(car, car2, car3, car4, car5, car6, car7, car8, car9, car10);
 
@@ -57,5 +108,11 @@ public class Runner {
 
         System.out.println("Task a");
         taskA(reader, cars);
+
+        System.out.println("Task b");
+        taskB(reader, cars);
+
+        System.out.println("Task c");
+        taskC(reader, cars);
     }
 }
--- vehicle/src/main/java/entity/Car.java
+++ vehicle/src/main/java/entity/Car.java
@@ -3,14 +3,16 @@ package main.java.entity;
 public class Car {
   private int id;
   private String brand;
+  private String model;
   private int yearOfIssue;
   private String color;
   private int cost;
   private String registrationNumber;
 
-  public Car(int id, String brand, int yearOfIssue, String color, int cost, String registrationNumber){
+  public Car(int id, String brand, String model, int yearOfIssue, String color, int cost, String registrationNumber){
     this.id = id;
     this.brand = brand;
+    this.model = model;
     this.yearOfIssue = yearOfIssue;
     this.color = color;
     this.cost = cost;
@@ -18,33 +20,77 @@ public class Car {
   }
 
   public int getId(){
-    return this.id;
+    return id;
+  }
+
+  public void setId(int id){
+    this.id = id;
   }
 
   public String getBrand(){
-    return this.brand;
+    return brand;
+  }
+
+  public void setBrand(String brand){
+    this.brand = brand;
+  }
+
+  public String getModel(){
+    return model;
+  }
+
+  public void setModel(String model){
+    this.model = model;
   }
 
   public int getYearOfIssue(){
-    return this.yearOfIssue;
+    return yearOfIssue;
+  }
+
+  public void setYearOfIssue(int yearOfIssue){
+    this.yearOfIssue = yearOfIssue;
   }
 
   public String getColor(){
-    return this.color;
+    return color;
+  }
+
+  public void setColor(String color){
+    this.color = color;
   }
 
   public int getCost(){
-    return this.cost;
+    return cost;
   }
 
-  public String getRregistrationNumber(){
-    return this.registrationNumber;
+  public void setCost(int cost){
+    this.cost = cost;
+  }
+
+  public String getRegistrationNumber(){
+    return registrationNumber;
+  }
+
+  public void setRegistrationNumber(String registrationNumber){
+    this.registrationNumber = registrationNumber;
   }
 
   public void print(){
     System.out.printf(
-            "main.java.entity.Car [\nid: %d. \nbrand: %s \nyear of issue: %d \ncolor: %s \ncost: %d \nregistration number: %s \n]%n",
+            "[\nid: %d. \nbrand: %s \nyear of issue: %d \ncolor: %s \ncost: %d \nregistration number: %s \n]%n",
       this.id, this.brand, this.yearOfIssue, this.color, this.cost, this.registrationNumber
     );
   }
+
+  @Override
+  public String toString(){
+    return "Car id: " + id +
+            "\tBrand: " + brand + ", " +
+            "\tModel: " + model + ", " +
+            "\tYear Of Issue: " + yearOfIssue + ", " +
+            "\tColor: " + color + ", " +
+            "\tCost: " + cost   + ", " +
+            "\tRegistration number: " + registrationNumber;
+  }
+
 }
